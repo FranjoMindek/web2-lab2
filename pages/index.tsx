905c17081e353f73b1d9ecf968b664dfde9c5342
Franjo Mindek <franjo.mindek@neos.hr>
2022-11-05 20:05:03 +0100

    2. zadatak i fly.io pokusaj pocketbase koji ne radi

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,24 @@ import Head from 'next/head'
 import { setCookie, getCookie, deleteCookie } from 'cookies-next'
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import PocketBase from 'pocketbase';
 
 export default function Home() {
 
   const [text, setText] = useState('');
   const [safeInput, setSafeInput] = useState(false);
-  const [safeCookie, setSafeCookie] = useState(false);
+  const [safeRegister, setSafeRegister] = useState(false);
+  const [safeLogin, setSafeLogin] = useState(false);
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [usernameInfo, setUsernameInfo] = useState('');
+  const [passwordInfo, setPasswordInfo] = useState('');
 
   const router = useRouter()
+  const client = new PocketBase('http://127.0.0.1:8090');
+  const bcrypt = require('bcryptjs');
 
   useEffect( () => {
     const inputData = localStorage.getItem("safeInput");
@@ -18,20 +28,7 @@ export default function Home() {
     } else {
       insecureInput();
     }
-
     setCookie("insecureCookie", "IdontHaveHttpOnlyFlag");
-    // const cookieData = localStorage.getItem("safeCookie");
-    // if (cookieData !== null) {
-    //   const cookieSafe = JSON.parse(cookieData) as boolean;
-    //   setSafeCookie(cookieSafe)
-    //   if (cookieSafe) {
-    //     secureCookie();
-    //   } else {
-    //     insecureCookie();
-    //   }
-    // } else {
-    //   insecureCookie();
-    // }
   }, []);
 
   useEffect( () => {
@@ -48,19 +45,97 @@ export default function Home() {
     setSafeInput(false);
   }
 
-  // function secureCookie() {
-  //   if(getCookie("insecureCookie"))
-  //     deleteCookie("insecureCookie");
-  //   setCookie("secureCookie", "IHaveHttpOnlyFlag", {httpOnly: true})
-  //   localStorage.setItem("safeCookie", "true");
-  // }
+  async function createInsecureUser() {
+    const data = {username: registerUsername, password: registerPassword};
+    try {
+      const record = await client.records.create('insecure_user', data);
+      setRegisterUsername('');
+      setRegisterPassword('');
+      console.log("Created user: ", record);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // function insecureCookie() {
-  //   if(getCookie("secureCookie"))
-  //     deleteCookie("secureCookie");
-  //   setCookie("insecureCookie", "IdontHaveHttpOnlyFlag");
-  //   localStorage.setItem("safeCookie", "false");
-  // }
+  async function createSecureUser() {
+    if (registerPassword.length < 12) {
+      const textShake = [
+        { transform: 'scale(1)' },
+        { transform: 'scale(1.1)'},
+        { transform: 'scale(1)'},
+        { transform: 'scale(0.9)'},
+        { transform: 'scale(1)' }
+      ];
+      const textTiming = {
+        duration: 300,
+        iterations: 2,
+      };
+      const condition = document.getElementById("passCondition")
+      condition!.animate(textShake, textTiming);
+      return;
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(registerPassword, salt);
+    const data = {username: registerUsername, hash};
+    try {
+      const record = await client.records.create('secure_user', data);
+      setRegisterUsername('');
+      setRegisterPassword('');
+      console.log("Created user: ", record);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function loginInsecureUser() {
+    const records = await client.records.getFullList('insecure_user', 200, {
+      sort: '-created',
+    });
+    const insecureUser = records.find(insecureUser => insecureUser.username == loginUsername);
+    if (insecureUser === undefined) {
+      setUsernameInfo('Nepostojano korisničko ime');
+      setPasswordInfo('');
+    }
+    else if (insecureUser.password !== loginPassword) {
+      setUsernameInfo('');
+      setPasswordInfo('Netočna loznika za korisničko ime');
+    } else {
+      setUsernameInfo('');
+      setPasswordInfo('');
+      setLoginUsername('');
+      setLoginPassword('');
+      alert("Uspješna prijava!");
+    }
+  }
+
+  async function loginSecureUser() {
+    const records = await client.records.getFullList('secure_user', 200, {
+      sort: '-created',
+    });
+    const secureUser = records.find(secureUser => bcrypt.compareSync(loginPassword, secureUser.hash));
+    if (secureUser === undefined) {
+      setUsernameInfo('Netočno korisničko ime ili loznika');
+      setPasswordInfo('');
+    } else {
+      setUsernameInfo('');
+      setPasswordInfo('');
+      setLoginUsername('');
+      setLoginPassword('');
+      alert("Uspješna prijava!");
+    }
+  }
+
+  function toSecureLogin() {
+    setSafeLogin(true);
+    setUsernameInfo('');
+    setPasswordInfo('');
+  }
+
+  function toInsecureLogin() {
+    setSafeLogin(false);
+    setUsernameInfo('');
+    setPasswordInfo('');
+  }
 
   return (
     <div className=''>
@@ -76,15 +151,12 @@ export default function Home() {
         <div>
           <p className='italic'>1. zadatak</p>
           <div className='container max-w-screen-md mx-auto bg-gray-400'>
-            <h2 className='text-xl p-2'>Check out this ELEMENT!!! Its made from website query parameter text!!! I learned about urls yesterday!!</h2>
+            <h2 className='text-xl p-2'>Check out this ELEMENT!!! Its made from website query parameter text!!! I learned about urls {safeInput ? "2 days ago" : "yesterday"}!!</h2>
             <div className='flex flex-col'>
-              {/* <input type="text" value={text} onChange={handleChange}></input> */}
               {!safeInput && <div className='py-5 bg-gray-300' dangerouslySetInnerHTML={{__html: text}}></div>}
               {safeInput && <div className='py-5 bg-gray-300'>{text}</div>}
-              {/* <button className='bg-red-200' onClick={insecureCookie}>Set a insecure cookie</button>
-              <button className='bg-green-200' onClick={secureCookie}>Set a secure cookie</button> */}
-              <button className='bg-red-200' style={!safeInput ? {borderWidth: 3, borderColor: "red"} : {}} onClick={insecureInput}>Set a insecure input</button>
-              <button className='bg-green-200' style={safeInput ? {borderWidth: 3, borderColor: "green"} : {}} onClick={secureInput}>Set a secure input</button>
+              <button className='bg-red-200' style={!safeInput ? {borderWidth: 2, borderColor: "red"} : {}} onClick={insecureInput}>Set a insecure input</button>
+              <button className='bg-green-200' style={safeInput ? {borderWidth: 2, borderColor: "green"} : {}} onClick={secureInput}>Set a secure input</button>
             </div>
           </div>
         </div>
@@ -93,15 +165,38 @@ export default function Home() {
         <div>
           <p className='italic'>2. zadatak</p>
           <div className='container max-w-screen-md mx-auto bg-gray-400'>
-            <h2 className='text-xl p-2'>Check out this ELEMENT!!! Its made from website query parameter text!!! I learned about urls yesterday!!</h2>
-            <div className='flex flex-col'>
-              {/* <input type="text" value={text} onChange={handleChange}></input> */}
-              {!safeInput && <div className='py-5 bg-gray-300' dangerouslySetInnerHTML={{__html: text}}></div>}
-              {safeInput && <div className='py-5 bg-gray-300'>{text}</div>}
-              {/* <button className='bg-red-200' onClick={insecureCookie}>Set a insecure cookie</button>
-              <button className='bg-green-200' onClick={secureCookie}>Set a secure cookie</button> */}
-              <button className='bg-red-200' style={!safeInput ? {borderWidth: 3, borderColor: "red"} : {}} onClick={insecureInput}>Set a insecure input</button>
-              <button className='bg-green-200' style={safeInput ? {borderWidth: 3, borderColor: "green"} : {}} onClick={secureInput}>Set a secure input</button>
+            <h2 className='text-xl p-2'>Super cool registration {safeRegister ? <del>without</del> : "without"} the hassle of long passwords!!</h2>
+            <div className='flex flex-col text-left bg-gray-300'>
+              <ul className='flex px-2 mt-2'>
+                <li className='w-1/5'>Korisničko ime:</li>
+                <li className='w-2/5'><input className='w-full' type="text" value={registerUsername} onChange={(e) => setRegisterUsername(e.target.value)}></input></li>
+              </ul>
+              <ul className='flex px-2 mt-2'>
+                <li className='w-1/5'>Lozinka:</li>
+                <li className='w-2/5'><input className='w-full' type={safeRegister ? "password" : "text"} value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)}></input></li>
+                <li id="passCondition" className='w-2/5 pl-2'>Uvjet: {safeRegister ? `Minimalno 12 znakova - ${registerPassword.length}/12` : "Nema"}</li>
+              </ul>
+              <button className="my-2 bg-blue-200 mx-auto p-2 rounded-xl border-2 border-blue-500" onClick={() => safeRegister ? createSecureUser() : createInsecureUser()}>Registriraj se!</button>
+              <button className='bg-red-200'   style={!safeRegister ? {borderWidth: 2, borderColor: "red"}   : {}} onClick={() => setSafeRegister(false)}>Set a insecure register</button>
+              <button className='bg-green-200' style={ safeRegister ? {borderWidth: 2, borderColor: "green"} : {}} onClick={() => setSafeRegister(true)}>Set a secure register</button>
+            </div>
+          </div>
+          <div className='container max-w-screen-md mx-auto bg-gray-400'>
+            <h2 className='text-xl p-2'>Super cool login which {safeLogin ? "doesn't remind hackers" : "reminds you"} of your login data!!</h2>
+            <div className='flex flex-col text-left bg-gray-300'>
+              <ul className='flex px-2 mt-2'>
+                <li className='w-1/5'>Korisničko ime:</li>
+                <li className='w-2/5'><input className='w-full' type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)}></input></li>
+                <li id="userInfo" className='w-2/5 pl-2'>{usernameInfo}</li>
+              </ul>
+              <ul className='flex px-2 mt-2'>
+                <li className='w-1/5'>Lozinka:</li>
+                <li className='w-2/5'><input className='w-full' type={safeLogin ? "password" : "text"} value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}></input></li>
+                <li id="passInfo" className='w-2/5 pl-2'>{passwordInfo}</li>
+              </ul>
+              <button className="my-2 bg-blue-200 mx-auto p-2 rounded-xl border-2 border-blue-500" onClick={() => safeLogin ? loginSecureUser() : loginInsecureUser()}>Prijavi se!</button>
+              <button className='bg-red-200'   style={!safeLogin ? {borderWidth: 2, borderColor: "red"}   : {}} onClick={toInsecureLogin}>Set a insecure login</button>
+              <button className='bg-green-200' style={ safeLogin ? {borderWidth: 2, borderColor: "green"} : {}} onClick={toSecureLogin}>Set a secure login</button>
             </div>
           </div>
         </div>
